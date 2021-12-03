53245869a5991a1efc4e7c3cf1325aeff3371608
Zhang He <zhanghe@zhe.cool>
2021-12-03 15:19:56 +0800

    feat(zsh): 增加初始化脚本

---
+++ zsh-shortcut-fe-work.plugin.zsh
@@ -0,0 +1,2 @@
+alias fenpminit='npm install -g typescript serverless yarn nrm commitizen cz-conventional-changelog'
+alias febrewinit='brew install spectacle keka clashx-pro font-cascadia-code visual-studio-code microsoft-edge google-chrome forecast iina obs obsidian motrix'
